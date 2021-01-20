6b5d182771c767c70bc3251d5b45a9dc6b39ad18
Wendi Magee <wendimagee@gmail.com>
2021-01-20 14:47:10 -0500

    Initial commit

---
+++ DecisionMaking/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DecisionMaking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello User! Please enter a number between 1 and 100");
+            int number = Int32.Parse(Console.ReadLine());
+            
+            if (number % 2 == 0 )
+            {
+                Console.WriteLine("The number is even");
+            }
+            else
+            {
+                Console.WriteLine(number + "The number is odd");
+            }
+
+
+
+        }
+    }
+}
