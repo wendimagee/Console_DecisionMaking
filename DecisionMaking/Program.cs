87b8a48a729159c4fb0500bd8c4ec21ca8d39651
Wendi Magee <wendimagee@gmail.com>
2021-01-20 19:36:09 -0500

    Built the program! I moved the whole project to a PC and now i can commit changes

--- DecisionMaking/Program.cs
+++ DecisionMaking/Program.cs
@@ -6,16 +6,31 @@ namespace DecisionMaking
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello User! Please enter a number between 1 and 100");
+            Console.WriteLine("Hi there! What's your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " +name +"! Please enter a number between 1 and 100");
             int number = Int32.Parse(Console.ReadLine());
             
-            if (number % 2 == 0 )
+            if(number < 1 || number > 100)
             {
-                Console.WriteLine("The number is even");
+                Console.WriteLine(name + ", that's an invalid entry. Try again");
+            }
+                
+            else if(number % 2 == 0 && number<25)
+            {
+                Console.WriteLine("Even and less than 25.");
+            }
+            else if (number % 2 == 0 && number > 25 && number<= 60)
+            {
+                Console.WriteLine("Even");
+            }
+            else if (number % 2 == 0 && number > 60)
+            {
+                Console.WriteLine(number + " Even");
             }
             else
             {
-                Console.WriteLine(number + "The number is odd");
+                Console.WriteLine(number + " The number is odd");
             }
 
 
