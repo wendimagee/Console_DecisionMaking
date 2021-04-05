dd7eada18297139ce06a7e1f7762b7bdb9256301
Wendi Magee <wendimagee@gmail.com>
2021-04-05 09:42:44 -0400

    I refactored this program to catch invalid inputs. I also refactored to help program embody DRY principle.

--- DecisionMaking/Program.cs
+++ DecisionMaking/Program.cs
@@ -8,33 +8,56 @@ namespace DecisionMaking
         {
             Console.WriteLine("Hi there! What's your name?");
             string name = Console.ReadLine();
-            Console.WriteLine("Hello, " +name +"! Please enter a number between 1 and 100");
-            int number = Int32.Parse(Console.ReadLine());
-            
-            if(number < 1 || number > 100)
+            bool goAgain = true;
+            do
             {
-                Console.WriteLine(name + ", that's an invalid entry. Try again");
-            }
-                
-            else if(number % 2 == 0 && number<25)
-            {
-                Console.WriteLine("Even and less than 25.");
-            }
-            else if (number % 2 == 0 && number > 25 && number<= 60)
-            {
-                Console.WriteLine("Even");
-            }
-            else if (number % 2 == 0 && number > 60)
-            {
-                Console.WriteLine(number + " Even");
-            }
-            else
-            {
-                Console.WriteLine(number + " The number is odd");
-            }
+                try
+                {
+                    Console.WriteLine("Hello, " + name + "! Please enter a number between 1 and 100");
+                    int number = Int32.Parse(Console.ReadLine());
+                    if (number < 1 || number > 100)
+                    {
+                        throw new Exception();
+                    }
 
+                    else if (number % 2 == 0)
+                    {
+                        if (number < 25)
+                        {
+                            Console.WriteLine("Even and less than 25.");
+                        }
+                        else if (number > 25 && number <= 60)
+                        {
+                            Console.WriteLine("Even");
+                        }
 
-
+                        else if (number > 60)
+                        {
+                            Console.WriteLine(number + " Even");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine(number + " The number is odd");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine(name + ", that's an invalid entry. Try again");
+                    goAgain = true;
+                }
+                Console.WriteLine("\n\nWould you like to check another number?(y/n)");
+                char answer = char.Parse(Console.ReadLine());
+                if (char.ToLower(answer) == 'y')
+                {
+                    goAgain = true;
+                }
+                else
+                {
+                    goAgain = false;
+                }
+            }
+            while (goAgain == true);
         }
     }
 }
